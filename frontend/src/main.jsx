b71e81975d1a4c8369ad70bb6be093c1fbcfe58a
lehoanganh75 <gaokun2021@gmail.com>
2026-02-05 18:01:26 +0700

    Fix CI: update node version and fix import path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { NotificationProvider } from './context/NotificationContext';
 
 if (import.meta.env.PROD) {
   console.log = () => {};
@@ -15,8 +14,6 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <NotificationProvider>
       <App />
-    </NotificationProvider>
   </React.StrictMode>
 );
