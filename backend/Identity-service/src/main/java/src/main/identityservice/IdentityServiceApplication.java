e202e08d70b52bb6b1c592b4d2b128cf867e3e8b
Lê Hoàng Anh <111717015+lehoanganh75@users.noreply.github.com>
2026-03-25 13:51:57 +0700

    Update IdentityServiceApplication.java

--- backend/Identity-service/src/main/java/src/main/identityservice/IdentityServiceApplication.java
+++ backend/Identity-service/src/main/java/src/main/identityservice/IdentityServiceApplication.java
@@ -3,9 +3,11 @@ package src.main.identityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"src.main.identityservice"})
+@RestController
 public class IdentityServiceApplication {
 
     public static void main(String[] args) {
