e0612a22c51b2d78699ffd05709dd677f6d6e7cb
Lê Hoàng Anh <111717015+lehoanganh75@users.noreply.github.com>
2026-03-25 12:52:32 +0700

    Update SecurityConfig.java

--- backend/Identity-service/src/main/java/src/main/identityservice/config/SecurityConfig.java
+++ backend/Identity-service/src/main/java/src/main/identityservice/config/SecurityConfig.java
@@ -42,8 +42,8 @@ public class SecurityConfig {
                 .sessionManagement(ss -> ss.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/admin/**").permitAll()
                         .requestMatchers("/api/profiles/**").permitAll()
                         .anyRequest().authenticated()
                 )
